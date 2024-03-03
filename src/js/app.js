8bfc9313cf2e72922ce15827fe5f9e42f46c7773
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-03-03 15:57:09 +0100

    AddProduct functional

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,53 @@
 import {validateInput} from './validateForm'
 
 //ELEMENTS FROM DOM
+// const idInput = document.querySelector('.id-input');
+// const quantityInput = document.querySelector('.quantity-input');
 const nameInput = document.querySelector('.name-input');
-const idInput = document.querySelector('.id-input');
 const manufacturerInput = document.querySelector('.manufacturer-input');
 const expirationDateInput = document.querySelector('.expiration-date-input');
-const quantityInput = document.querySelector('.quantity-input');
-
 const selectInput = document.querySelector('.select-input');
 const volumeInput = document.querySelector('.volume-input');
 const contentInput = document.querySelector('.content-input');
 
 const errorMsg = document.querySelector('.error-message');
 
+const submitForm = document.querySelector('.input-form');
 const submitButton = document.querySelector('.submit-button');
 
 //ARRAYS
-const products = [];
 const liquidProducts = [];
+const capsuleProducts = [];
 
-//FORM VALIDATION
-submitButton.addEventListener('click', (e)=>{
+//ADDEVENTLISTENERS
+submitForm.addEventListener('submit', (e)=>{
 	e.preventDefault();
-	validateInput(nameInput.value, manufacturerInput.value, expirationDateInput.value, selectInput.value, volumeInput.value, contentInput.value, errorMsg);
+	let newProduct;
+	if (selectInput.value === 'liquid'){
+		newProduct = new Product(
+			nameInput.value,
+			manufacturerInput.value,
+			expirationDateInput.value,
+			selectInput.value,
+			volumeInput.value
+		);
+	} else if (selectInput.value === 'capsule') {
+		newProduct = new CapsuleProduct(
+			nameInput.value,
+			manufacturerInput.value,
+			expirationDateInput.value,
+			selectInput.value,
+			contentInput.value
+		);
+	}
+	Product.addProduct(newProduct);
+	console.log(newProduct);
 })
+
+// submitButton.addEventListener('click', (e)=>{
+// 	e.preventDefault();
+// 	validateInput(nameInput.value, manufacturerInput.value, expirationDateInput.value, selectInput.value, volumeInput.value, contentInput.value, errorMsg);
+// })
 
 selectInput.addEventListener('change', ()=>{
 	if(selectInput.value === 'liquid'){
@@ -37,25 +61,29 @@ selectInput.addEventListener('change', ()=>{
 	}
 })
 
-CLASSES
+// CLASSES
 class Product {
-	constructor(name, manufacturer, date, quantity, volume){
+	constructor(name, manufacturer, expirationDate, form, quantityMl){
 		this.name = name;
 		this.manufacturer = manufacturer;
-		this.date = date;
-		this.quantity = quantity;
-		this.volume = volume;
+		this.expirationDate = expirationDate;
+		this.form = form;
+		this.quantityMl = quantityMl;
 		this.ID = Date.now();
 	}
 	static addProduct(product) {
-		
+		if(product.form === 'liquid'){
+			liquidProducts.push(product);
+		} else if (product.form === 'capsule') {
+			capsuleProducts.push(product);
+		}
 	}
 }
 
-class LiquidProduct extends Product {
-	constructor(name, manufacturer, date, quantity, content){
-		super(name, manufacturer, date, quantity)
-		this.content = content;
+class CapsuleProduct extends Product {
+	constructor(name, manufacturer, expirationDate, form, quantityMg){
+		super(name, manufacturer, expirationDate, form)
+		this.quantityMg = quantityMg;
 		this.ID = Date.now();
 	}
 }
